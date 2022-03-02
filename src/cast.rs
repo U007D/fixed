b0eb5a846998f2fe4508c735fca651c164c7335a
Trevor Spiteri <tspiteri@ieee.org>
2022-03-02 12:59:46 +0100

    remove strict constraints from az conversions

--- src/cast.rs
+++ src/cast.rs
@@ -14,7 +14,6 @@
 // <https://opensource.org/licenses/MIT>.
 
 use crate::{
-    types::extra::{If, True},
     F128Bits, FixedI128, FixedI16, FixedI32, FixedI64, FixedI8, FixedU128, FixedU16, FixedU32,
     FixedU64, FixedU8,
 };
@@ -23,11 +22,7 @@ use half::{bf16, f16};
 
 macro_rules! cast {
     ($Src:ident($nbits_src:expr); $Dst:ident($nbits_dst:expr)) => {
-        impl<const SRC_FRAC: i32, const DST_FRAC: i32> Cast<$Dst<DST_FRAC>> for $Src<SRC_FRAC>
-        where
-            If<{ (0 <= SRC_FRAC) & (SRC_FRAC <= $nbits_src) }>: True,
-            If<{ (0 <= DST_FRAC) & (DST_FRAC <= $nbits_dst) }>: True,
-        {
+        impl<const SRC_FRAC: i32, const DST_FRAC: i32> Cast<$Dst<DST_FRAC>> for $Src<SRC_FRAC> {
             #[inline]
             fn cast(self) -> $Dst<DST_FRAC> {
                 self.to_num()
@@ -36,9 +31,6 @@ macro_rules! cast {
 
         impl<const SRC_FRAC: i32, const DST_FRAC: i32> CheckedCast<$Dst<DST_FRAC>>
             for $Src<SRC_FRAC>
-        where
-            If<{ (0 <= SRC_FRAC) & (SRC_FRAC <= $nbits_src) }>: True,
-            If<{ (0 <= DST_FRAC) & (DST_FRAC <= $nbits_dst) }>: True,
         {
             #[inline]
             fn checked_cast(self) -> Option<$Dst<DST_FRAC>> {
@@ -48,9 +40,6 @@ macro_rules! cast {
 
         impl<const SRC_FRAC: i32, const DST_FRAC: i32> SaturatingCast<$Dst<DST_FRAC>>
             for $Src<SRC_FRAC>
-        where
-            If<{ (0 <= SRC_FRAC) & (SRC_FRAC <= $nbits_src) }>: True,
-            If<{ (0 <= DST_FRAC) & (DST_FRAC <= $nbits_dst) }>: True,
         {
             #[inline]
             fn saturating_cast(self) -> $Dst<DST_FRAC> {
@@ -60,9 +49,6 @@ macro_rules! cast {
 
         impl<const SRC_FRAC: i32, const DST_FRAC: i32> WrappingCast<$Dst<DST_FRAC>>
             for $Src<SRC_FRAC>
-        where
-            If<{ (0 <= SRC_FRAC) & (SRC_FRAC <= $nbits_src) }>: True,
-            If<{ (0 <= DST_FRAC) & (DST_FRAC <= $nbits_dst) }>: True,
         {
             #[inline]
             fn wrapping_cast(self) -> $Dst<DST_FRAC> {
@@ -72,9 +58,6 @@ macro_rules! cast {
 
         impl<const SRC_FRAC: i32, const DST_FRAC: i32> OverflowingCast<$Dst<DST_FRAC>>
             for $Src<SRC_FRAC>
-        where
-            If<{ (0 <= SRC_FRAC) & (SRC_FRAC <= $nbits_src) }>: True,
-            If<{ (0 <= DST_FRAC) & (DST_FRAC <= $nbits_dst) }>: True,
         {
             #[inline]
             fn overflowing_cast(self) -> ($Dst<DST_FRAC>, bool) {
@@ -84,9 +67,6 @@ macro_rules! cast {
 
         impl<const SRC_FRAC: i32, const DST_FRAC: i32> UnwrappedCast<$Dst<DST_FRAC>>
             for $Src<SRC_FRAC>
-        where
-            If<{ (0 <= SRC_FRAC) & (SRC_FRAC <= $nbits_src) }>: True,
-            If<{ (0 <= DST_FRAC) & (DST_FRAC <= $nbits_dst) }>: True,
         {
             #[inline]
             #[track_caller]
@@ -97,60 +77,42 @@ macro_rules! cast {
     };
 
     ($Fixed:ident($nbits:expr); $Dst:ident) => {
-        impl<const FRAC: i32> Cast<$Dst> for $Fixed<FRAC>
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> Cast<$Dst> for $Fixed<FRAC> {
             #[inline]
             fn cast(self) -> $Dst {
                 self.to_num()
             }
         }
 
-        impl<const FRAC: i32> CheckedCast<$Dst> for $Fixed<FRAC>
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> CheckedCast<$Dst> for $Fixed<FRAC> {
             #[inline]
             fn checked_cast(self) -> Option<$Dst> {
                 self.checked_to_num()
             }
         }
 
-        impl<const FRAC: i32> SaturatingCast<$Dst> for $Fixed<FRAC>
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> SaturatingCast<$Dst> for $Fixed<FRAC> {
             #[inline]
             fn saturating_cast(self) -> $Dst {
                 self.saturating_to_num()
             }
         }
 
-        impl<const FRAC: i32> WrappingCast<$Dst> for $Fixed<FRAC>
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> WrappingCast<$Dst> for $Fixed<FRAC> {
             #[inline]
             fn wrapping_cast(self) -> $Dst {
                 self.wrapping_to_num()
             }
         }
 
-        impl<const FRAC: i32> OverflowingCast<$Dst> for $Fixed<FRAC>
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> OverflowingCast<$Dst> for $Fixed<FRAC> {
             #[inline]
             fn overflowing_cast(self) -> ($Dst, bool) {
                 self.overflowing_to_num()
             }
         }
 
-        impl<const FRAC: i32> UnwrappedCast<$Dst> for $Fixed<FRAC>
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> UnwrappedCast<$Dst> for $Fixed<FRAC> {
             #[inline]
             #[track_caller]
             fn unwrapped_cast(self) -> $Dst {
@@ -160,60 +122,42 @@ macro_rules! cast {
     };
 
     ($Src:ident; $Fixed:ident($nbits:expr)) => {
-        impl<const FRAC: i32> Cast<$Fixed<FRAC>> for $Src
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> Cast<$Fixed<FRAC>> for $Src {
             #[inline]
             fn cast(self) -> $Fixed<FRAC> {
                 <$Fixed<FRAC>>::from_num(self)
             }
         }
 
-        impl<const FRAC: i32> CheckedCast<$Fixed<FRAC>> for $Src
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> CheckedCast<$Fixed<FRAC>> for $Src {
             #[inline]
             fn checked_cast(self) -> Option<$Fixed<FRAC>> {
                 <$Fixed<FRAC>>::checked_from_num(self)
             }
         }
 
-        impl<const FRAC: i32> SaturatingCast<$Fixed<FRAC>> for $Src
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> SaturatingCast<$Fixed<FRAC>> for $Src {
             #[inline]
             fn saturating_cast(self) -> $Fixed<FRAC> {
                 <$Fixed<FRAC>>::saturating_from_num(self)
             }
         }
 
-        impl<const FRAC: i32> WrappingCast<$Fixed<FRAC>> for $Src
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> WrappingCast<$Fixed<FRAC>> for $Src {
             #[inline]
             fn wrapping_cast(self) -> $Fixed<FRAC> {
                 <$Fixed<FRAC>>::wrapping_from_num(self)
             }
         }
 
-        impl<const FRAC: i32> OverflowingCast<$Fixed<FRAC>> for $Src
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> OverflowingCast<$Fixed<FRAC>> for $Src {
             #[inline]
             fn overflowing_cast(self) -> ($Fixed<FRAC>, bool) {
                 <$Fixed<FRAC>>::overflowing_from_num(self)
             }
         }
 
-        impl<const FRAC: i32> UnwrappedCast<$Fixed<FRAC>> for $Src
-        where
-            If<{ (0 <= FRAC) & (FRAC <= $nbits) }>: True,
-        {
+        impl<const FRAC: i32> UnwrappedCast<$Fixed<FRAC>> for $Src {
             #[inline]
             #[track_caller]
             fn unwrapped_cast(self) -> $Fixed<FRAC> {
