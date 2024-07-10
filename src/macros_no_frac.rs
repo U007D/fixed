90b88ed0efddfac87ab1b3787dda327016240af6
Trevor Spiteri <tspiteri@ieee.org>
2024-07-10 14:27:34 +0200

    doc: hypot example was using overflowing_hypot instead

--- src/macros_no_frac.rs
+++ src/macros_no_frac.rs
@@ -1866,7 +1866,7 @@ type Fix = ", stringify!($Self), "<", stringify!($nm4), ">;
 
 // hypot(3, 4) == 5
 assert_eq!(
-    Fix::from_num(3).overflowing_hypot(Fix::from_num(4)),
+    Fix::from_num(3).hypot(Fix::from_num(4)),
     (Fix::from_num(5), false)
 );
 ```
